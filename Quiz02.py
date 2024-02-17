09b69a04d2d6858e8dcb822bca3c923eae82424f
orastean22 <98747962+orastean22@users.noreply.github.com>
2024-02-17 18:33:39 +0100

    add Quiz02 (17.02.2024)

---
+++ Quiz02.py
@@ -0,0 +1,22 @@
+# 1.Take input of a, r and n
+a = int(input("Enter the value of a: "))
+r = int(input("Enter the value of a: "))
+n = int(input("Enter the value of a: "))
+
+# 2.Loop for n terms
+for i in range(1, n+1):
+    t_n = a * r**(i-1)
+    print(t_n)
+
+# Output 
+# Enter the value for a =1
+# Enter the value for r =2
+# 1
+# 2
+# 4
+# 8
+# 16
+# 32
+# 64
+#....
+    
