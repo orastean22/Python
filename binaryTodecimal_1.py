310aed883f3f3919cd171a1b76234be5c530c5f4
orastean22 <98747962+orastean22@users.noreply.github.com>
2024-03-01 17:10:46 +0100

    binaryTodecimal 01.03.2024

---
+++ binaryTodecimal_1.py
@@ -0,0 +1,15 @@
+def binary_to_decimal(binary_num):
+    decimal_num = 0
+    power = len(binary_num) - 1
+    
+    for digit in binary_num:
+        decimal_num += int(digit) * (2 ** power)
+        power -= 1
+    
+    return decimal_num
+
+# Example usage:
+# n = int(input("Enter the value of n in binar : "))
+binary_num = "1101"
+decimal_num = binary_to_decimal(binary_num)
+print("Decimal representation of", binary_num, "is:", decimal_num)
