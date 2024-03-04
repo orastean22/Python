466ab64b33b3d11c178b4b731379343dc346cf01
orastean22 <98747962+orastean22@users.noreply.github.com>
2024-03-04 23:27:52 +0100

    04.03.2024 - add factorial using while loop

---
+++ Factrial_1.py
@@ -0,0 +1,9 @@
+counter = 5                            # Set the value to 5
+factorial = 1                          # Set the value to 5
+
+while counter > 1:                     # While counter(5) is greater than 0
+
+    factorial *= counter               # Set new value of factorial to counter.
+    counter -=1                        # Set the counter to counter - 1.
+
+print("Factorial = %d", factorial)     # Print the value of factorial.
