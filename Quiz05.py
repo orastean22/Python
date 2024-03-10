be219697169fb861371139e3e72aa9d5abb3f887
orastean22 <98747962+orastean22@users.noreply.github.com>
2024-03-10 20:29:22 +0100

    10.03.2024 add comment for Quiz05

--- Quiz05.py
+++ Quiz05.py
@@ -1,7 +1,7 @@
-def func(x=1, y=2):
-    x=x+y
-    y+=1
-    print(x,y)
+def func(x=1, y=2):   #define function func
+    x=x+y             #x=1+2
+    y+=1              #y will be 2+1
+    print(x,y)        
 func(y=2,x=1)
     #display - 3;3 
 
