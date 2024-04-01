1d745b19d40001e7e2b16598c2725e7a507ca592
orastean22 <98747962+orastean22@users.noreply.github.com>
2024-04-01 11:28:21 +0200

    01.04.2024 add Quiz12

---
+++ Quiz12.py
@@ -0,0 +1,24 @@
+print("Hello Pycharm")
+print(2**3 + (5+6) **(1+1))
+
+#display: 129
+
+
+""" Let's break down the expression step by step:
+1. `(5 + 6)` evaluates to `11`.
+2. `(1 + 1)` evaluates to `2`.
+3. `11 ** 2` equals `121`.
+4. `2 ** 3` equals `8`.
+Now, let's substitute these values back into the original expression:
+
+```
+print(8 + 121)
+```
+
+Finally, when you add `8` and `121`, you get:
+
+```
+129
+```
+
+So, the result of the expression `print(2**3 + (5+6) **(1+1))` is `129` """
