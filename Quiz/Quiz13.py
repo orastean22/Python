fd8e84994dd6cb95298a9a425de2f5d6213dad3e
orastean22 <98747962+orastean22@users.noreply.github.com>
2024-04-14 23:15:43 +0200

    14.04.2024 add Quiz13

---
+++ Quiz/Quiz13.py
@@ -0,0 +1,11 @@
+def func(x=1, y=2):
+    x=x+y
+    y+=1
+    print(x,y)
+func(y=2,x=1)
+
+#display 3 3
+#call func y=2 and x=1
+# x=(x=x+y)1+2 =>3
+# y=(y+=1)2+1 =>3  
+# if was func (y=5, x=1) => result was 6 6
