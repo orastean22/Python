4e936010e9c89c1dd4a28d7c389075e97aa01298
orastean22 <98747962+orastean22@users.noreply.github.com>
2024-04-17 20:12:55 +0200

    17.04.2024 add Quiz16

---
+++ Quiz/Quiz16.py
@@ -0,0 +1,16 @@
+text = "hello word"
+result=text.swapcase() #method converts all uppercase characters to lowercase and vice versa
+print(result)
+
+#display HELLO WORD
+
+def multiply_numbers(a,b):
+    result = a * b
+    return result
+    
+x = 5
+y = 3
+z = multiply_numbers(x,y)
+print(z)
+
+#display 15
