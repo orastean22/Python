b43a8c705384711093d2b3be060f255b90427ca5
adrianorastean <orastean22@yahoo.com>
2024-05-02 22:31:14 +0200

    02.05.2024 add Colorama example in Algo

---
+++ Algorithms/ColorText_inPython.py
@@ -0,0 +1,6 @@
+from colorama import Fore
+print(Fore.RED + "hello word")
+print(Fore.BLUE + "hello word")
+print(Fore.GREEN + "hello word")
+print(Fore.YELLOW + "hello word")
+print(Fore.CYAN + "THANK YOU")
