913844f5671c4067a42e31f87ef415b585028d45
adrianorastean <orastean22@yahoo.com>
2024-05-16 23:53:46 +0200

    add update and high resolution to download youtube videos

--- Algorithms/Download_YouTube_Video.py
+++ Algorithms/Download_YouTube_Video.py
@@ -2,9 +2,21 @@
 # need to open terminal and install this:    pip install yt-dlp
 
 import yt_dlp
-url = input("Enter video url: ")
-ydl_opts = {}
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
 
-print("Video download successfully!")
+# Specify the output directory where the video will be saved
+output_directory = '/Users/adrianorastean/Downloads/VIDEOCLIPS/'
+
+# Prompt the user for the video URL
+url = input("Enter video URL: ")
+
+# Set the options for youtube-dl
+ydl_opts = {
+    'outtmpl': output_directory + '%(title)s.%(ext)s'  # Set the output template
+}
+
+# Download the video to the specified location
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])  # Pass the URL as a list to download a single video
+
+print("Video downloaded successfully to:", output_directory)
+
---
+++ Algorithms/Download_YouTube_video-FULLHD.py
@@ -0,0 +1,36 @@
+#download youtube video using python scrypt on Highest resolutio
+"""  first install this library:  pip install pytube"""
+
+import yt_dlp
+
+def download_video(url, output_path):
+    try:
+        # Set options for downloading the highest resolution video
+        options = {
+            'format': 'bestvideo[height<=?1080][fps<=?30]+bestaudio/best[height<=?1080][fps<=?30]',
+            'outtmpl': output_path + '/%(title)s.%(ext)s',
+            'ffmpeg_location': '/opt/homebrew/bin/ffmpeg'  # Modify this path as needed
+        }
+
+        # Create a YoutubeDL object with options
+        ydl = yt_dlp.YoutubeDL(options)
+
+        # Download the video
+        with ydl:
+            result = ydl.extract_info(url, download=True)
+
+        print("Download complete.")
+    except Exception as e:
+        print("An error occurred:", e)
+
+
+if __name__ == "__main__":
+    # Ask the user to input the YouTube URL and output path
+    url = input("Enter the YouTube URL: ")
+    output_path = '/Users/adrianorastean/Downloads/VIDEOCLIPS/'
+
+    download_video(url, output_path)
+
+
+
+
