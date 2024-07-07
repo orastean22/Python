c4f1eab81e8be2c106c22e3c5978a25eac0aea86
adrianorastean <orastean22@yahoo.com>
2024-07-07 12:36:33 +0200

    Add my1st project

---
+++ Projects/my1stPrj/file2.py
@@ -0,0 +1,3 @@
+# file2.py
+def function2():
+    print("This is function2 from file2.")
---
+++ Projects/my1stPrj/main.py
@@ -0,0 +1,15 @@
+# main.py
+from file1 import function1
+from file2 import function2
+
+# you can also import like this
+# main.py
+# from my_package.file1 import function1
+# from my_package.file2 import function2
+
+def main():
+    function1()
+    function2()
+
+if __name__ == "__main__":
+    main()
