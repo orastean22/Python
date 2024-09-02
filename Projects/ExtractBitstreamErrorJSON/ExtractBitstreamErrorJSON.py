e7c8ec365b03180ffabc8f7e46a0bc9e095436a9
orastean22 <98747962+orastean22@users.noreply.github.com>
2024-09-02 16:21:53 +0200

    update from dell pc

--- Projects/ExtractBitstreamErrorJSON/ExtractBitstreamErrorJSON.py
+++ Projects/ExtractBitstreamErrorJSON/ExtractBitstreamErrorJSON.py
@@ -1,7 +1,15 @@
+#----------------------------------------------------------------------------------------------------------------------
+#-- Python Script File
+#-- Created on 09/Sept/2024 
+#-- Author: AdrianO
+#-- Comment: extract from Json file all errors and count them
+#-- Load the JSON data from the file
+#-- Copy the JSON file in the same folder with the script and run
+#----------------------------------------------------------------------------------------------------------------------
+
 import json
 from collections import defaultdict
-# Load the JSON data from the file
-# Copy the JSON file in the same folder with the script and run
+
 
 jsonName = "SIC2192Log_tempDev3_240829_120003.json"
 with open(jsonName, 'r') as file:
