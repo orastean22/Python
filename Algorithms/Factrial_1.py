86a85bd3a0fcfb08f43915f14f7796a7343a90f4
Orasteana <orastean22@yahoo.com>
2024-11-01 16:42:26 +0100

    update print result in float

--- Algorithms/Factrial_1.py
+++ Algorithms/Factrial_1.py
@@ -6,4 +6,4 @@ while counter > 1:                     # While counter(5) is greater than 0
     factorial *= counter               # Set new value of factorial to counter.
     counter -=1                        # Set the counter to counter - 1.
 
-print("Factorial = %d", factorial)     # Print the value of factorial.
+print(f"Factorial = ", factorial)     # Print the value of factorial.
