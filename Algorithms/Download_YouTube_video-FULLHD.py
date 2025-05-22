7d1dd8db493edf43dd3500608b95dcbaf6221581
adrianorastean <orastean22@yahoo.com>
2025-05-22 20:25:20 +0200

    update for video with error download

--- Algorithms/Download_YouTube_video-FULLHD.py
+++ Algorithms/Download_YouTube_video-FULLHD.py
@@ -1,10 +1,7 @@
 #download youtube video using python scrypt on Highest resolution
-"""  first install this library:  pip install pytube"""
-""" also pip install yt-dlp """
-
+#install : pip install pytube; pip install yt-dlp; pip install -U yt-dlp; 
 
 import yt_dlp
-
 
 def download_video(url, output_path):
     try:
@@ -12,20 +9,21 @@ def download_video(url, output_path):
         options = {
             'format': 'bestvideo[height<=?1080][fps<=?30]+bestaudio/best[height<=?1080][fps<=?30]',
             'outtmpl': output_path + '/%(title)s.%(ext)s',
-            'ffmpeg_location': '/opt/homebrew/bin/ffmpeg'  # Modify this path as needed
+            'ffmpeg_location': '/opt/homebrew/bin/ffmpeg',  # Modify this path as needed
+            'http_headers': {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+                            }
         }
-
         # Create a YoutubeDL object with options
         ydl = yt_dlp.YoutubeDL(options)
 
         # Download the video
         with ydl:
             result = ydl.extract_info(url, download=True)
-
         print("Download complete.")
+    
     except Exception as e:
         print("An error occurred:", e)
-
 
 if __name__ == "__main__":
     # Ask the user to input the YouTube URL and output path
@@ -33,7 +31,6 @@ if __name__ == "__main__":
     # output_path = '/Users/adrianorastean/Desktop/VIDEOCLIPS/'
     # output_path = '/Users/adrianorastean/Desktop/Trading Time/'
     output_path = '/Users/adrianorastean/Desktop/AUDIOBooks/'
-
     download_video(url, output_path)
 
 
